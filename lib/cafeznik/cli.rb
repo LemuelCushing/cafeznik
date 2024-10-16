0ed03b7bfd49e0bd744c1e6217a9e81297687c6e
Gill Abarbanel <SeventhHorror@gmail.com>
2024-10-16 11:32:34 +0200

    Use Slop for opts; Improve error; Add limit warning; Add tree option

--- lib/cafeznik/cli.rb
+++ lib/cafeznik/cli.rb
@@ -1,63 +1,60 @@
 #!/usr/bin/env ruby
 
-require 'optparse'
+require 'slop'
 require 'tty-command'
 require 'octokit'
 require 'clipboard'
 require 'base64'
+require 'fileutils'
+require 'tempfile'
 require 'logger'
 
 module Cafeznik
   class CLI
+    MAX_LINES = 10_000
+    MAX_FILES = 20
     class << self
       def start(argv)
         @argv = argv
 
-        initialize_logger
+        logger.error("No repository provided. Use -r or --repo to specify a GitHub repository.") && exit(1) unless repo
+        logger.error("No files or directories found in the repository.") && exit(1) if repo_tree.empty?
 
-        raise ArgumentError, "No repository provided. Use -r or --repo to specify a GitHub repository." unless repo
-        raise ArgumentError, "No files or directories found in the repository." if repo_tree.empty?
-
-        selected_files = select_paths
-        copy_files_to_clipboard(selected_files)
+        select_files
+        copy_files_to_clipboard
       end
 
       private
 
       def options = @_options ||= parse_options(@argv)
       def repo = options[:repo]
-      def repo_tree = @_repo_tree ||= fetch_repo_tree 
+      def repo_tree = @_repo_tree ||= fetch_repo_tree
+      def selected_files = @selected_files ||= []
 
-      def initialize_logger
-        @logger = Logger.new($stdout)
-        @logger.level = options[:verbose] ? Logger::DEBUG : Logger::INFO
-        @logger.formatter = proc do |severity, _datetime, _progname, msg|
-          "[#{severity}] #{msg}\n"
+      def logger
+        @_logger ||= Logger.new($stdout).tap do |log|
+          log.level = options[:verbose] ? Logger::DEBUG : Logger::INFO
+          log.formatter = proc { |severity, _, _, msg| "[#{severity}] #{msg}\n" }
         end
       end
 
-      def logger = @logger
-
       def parse_options(argv)
-        options = { no_header: false, verbose: false }
-        OptionParser.new do |opts|
-          opts.banner = "Usage: cafeznik [options]"
-          opts.on("--no-header", "-nh", "Exclude headers from copied content") { options[:no_header] = true }
-          opts.on("-r", "--repo REPO", "GitHub repository (owner/repo format)") { |r| options[:repo] = r }
-          opts.on("-v", "--verbose", "Run in verbose mode") { options[:verbose] = true }
-          opts.on("-h", "--help", "Show this help message") {
-            puts opts
+        Slop.parse(argv) do |o|
+          o.string '-r', '--repo', 'GitHub repository (owner/repo format)', required: true
+          o.bool '-nh', '--no-header', 'Exclude headers from copied content'
+          o.bool '-tr', '--with-tree', 'Include the tree structure in the content'
+          o.bool '-v', '--verbose', 'Run in verbose mode'
+          o.on '-h', '--help' do
+            puts o
             exit
-          }
-        end.parse!(argv)
-        options
+          end
+        end
       end
 
       def github_token = @_github_token ||=
         ENV['GITHUB_TOKEN'] ||
         fetch_token_via_gh ||
-        (logger.error("GitHub token not found. Please configure `gh` or set GITHUB_TOKEN in your environment.") && exit(1))
-
+        (logger.error("GitHub token not found. Please configure `gh` or set GITHUB_TOKEN in your environment."); exit 1)
 
       def fetch_token_via_gh
         cmd = TTY::Command.new(printer: :null)
@@ -70,22 +67,22 @@ module Cafeznik
         nil
       end
 
-      def client =  @_client ||= Octokit::Client.new(
+      def client = @_client ||= Octokit::Client.new(
         access_token: github_token,
         auto_paginate: true
       )
-      
 
       def fetch_repo_tree
         default_branch = client.repository(repo).default_branch
-        tree = client.tree(repo, default_branch, recursive: true)
-        files = tree.tree.select { |item| item.type == 'blob' }.map(&:path)
-        directories = files.map { |path| File.dirname(path) }.uniq.reject { |dir| dir == '.' }
-        all_paths = files + directories.map { |dir| "#{dir}/" }
-        all_paths.sort
+        tree = client.tree(repo, default_branch, recursive: true).tree
+
+        files = tree.select { _1.type == 'blob' }.map(&:path)
+        directories = files.map { File.dirname(_1) + "/" }
+
+        (["./"] + files + directories).uniq.sort
       rescue Octokit::NotFound
-        logger.error("Repository not found: #{@repo}")
-        puts "Repository not found: #{@repo}"
+        logger.error("Repository not found: #{repo}")
+        puts "Repository not found: #{repo}"
         exit 1
       rescue Octokit::Error => e
         logger.error("Error fetching file tree: #{e.message}")
@@ -93,61 +90,73 @@ module Cafeznik
         exit 1
       end
 
-      def select_paths
-        logger.debug("Initiating item selection with fzf.")
+      def select_files
+        logger.debug("Initiating file selection with fzf.")
         cmd = TTY::Command.new(printer: options[:verbose] ? :pretty : :null)
-        fzf_input = repo_tree.map { |p| p.gsub("'", "\\\\'") }.join("\n")
+        fzf_input = repo_tree.join("\n")
         result = cmd.run("echo \"#{fzf_input}\" | fzf --multi")
-        selected_paths = result.out.strip.split("\n")
-        logger.info("User selected_paths #{selected_paths.size} item(s).")
-        expand_selected_paths(selected_paths)
-      rescue TTY::Command::ExitError
-        logger.info("fzf exited without selecting items.")
-        exit 0
-      rescue StandardError => e
-        raise StandardError, "Error selecting items: #{e.message}"
-      end
+        paths = result.out.strip.split("\n")
+        logger.info("User selected #{paths.size} item(s).")
 
-      def expand_selected_paths(selected_paths)
-        selected_paths.each_with_object([]) do |selection, selected_files|
-          if directory?(selection)
-            dir_path = selection.chomp('/')
-            matched_files = repo_tree.select { |path| path.start_with?("#{dir_path}/") && !path.end_with?('/') }
-            selected_files.concat(matched_files)
-            logger.debug("Expanded directory #{dir_path} to include #{matched_files.size} files.")
+        @selected_files = paths.flat_map do |item|
+          if item == "./"
+            repo_tree.reject(&method(:directory?))
           else
-            selected_files << selection
+            directory?(item) ? files_in_directory(item.chomp('/')) : item
           end
         end.uniq
+
+        if @selected_files.size > MAX_FILES
+          logger.warn("Warning: You selected more than #{MAX_FILES} files. Are you sure you want to continue? (y/N)")
+          exit 0 unless STDIN.gets.strip.downcase == 'y'
+        end
+        
+        logger.info("Resolved to #{@selected_files.size} file(s).")
+      rescue TTY::Command::ExitError
+        logger.info("No items selected. Exiting.")
+        exit 0
+      rescue StandardError => e
+        raise "Error selecting files: #{e.message}"
       end
 
-      def copy_files_to_clipboard(files)
-        contents = files.map do |file|
+      def files_in_directory(dir_path)
+        matched_files = repo_tree.select { _1.start_with?("#{dir_path}/") }.reject(&method(:directory?))
+        logger.debug("Expanded directory #{dir_path} to include #{matched_files.size} files.")
+        matched_files
+      end
+
+      def directory?(path) = path.end_with?('/')
+
+      def copy_files_to_clipboard
+        contents = selected_files.filter_map do |file|
           content = fetch_file_content(file)
           next unless content
 
-          content.prepend( "==> #{file} <==\n") unless options[:no_header]
+          content.prepend("==> #{file} <==\n") unless options[:no_header]
           content
-        end.compact.join("\n\n")
+        end.join("\n\n")
+
+        contents.prepend("==> File Tree <==\n #{repo_tree.join("\n")}\n\n") if options[:with_tree]
+
+        if contents.lines.size > MAX_LINES
+          puts "Warning: The total content exceeds #{MAX_LINES} lines. Are you sure you want to continue? (y/N)"
+          exit 0 unless STDIN.gets.strip.downcase == 'y'
+        end
 
         Clipboard.copy(contents)
-        logger.info("Copied #{files.size} file(s) to clipboard.")
-        puts "Copied #{files.size} file(s) to clipboard."
+        logger.info("Copied #{selected_files.size} file(s) to clipboard - #{contents.lines.size} line(s).")
       rescue Octokit::Error => e
         logger.error("Error copying files to clipboard: #{e.message}")
-        puts "Error copying files to clipboard: #{e.message}"
         exit 1
       end
 
       def fetch_file_content(path)
-        content = client.contents(repo, path:)[:content]
+        content = client.contents(repo, path: path)[:content]
         Base64.decode64(content)
       rescue Octokit::Error => e
         logger.error("Error fetching content for #{path}: #{e.message}")
         nil
       end
-
-      def directory?(selection) = selection.end_with?('/')
     end
   end
 end
