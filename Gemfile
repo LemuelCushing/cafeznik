0754f008a9923b15afccd142aeb69ddf29db980f
Gill Abarbanel <SeventhHorror@gmail.com>
2024-10-25 09:16:16 +0200

    respect gitignore when globbing locally

--- Gemfile
+++ Gemfile
@@ -1,18 +1,19 @@
-source 'https://rubygems.org'
+source "https://rubygems.org"
 
-gem 'tty-command'
-gem 'octokit'
-gem 'clipboard'
-gem 'base64'
-gem 'thor'
-gem 'faraday-retry'
-gem 'faraday-multipart'
+gem "base64"
+gem "clipboard"
+gem "faraday-multipart"
+gem "faraday-retry"
+gem "fast_ignore"
+gem "octokit"
+gem "thor"
+gem "tty-command"
 
 group :development, :test do
-  gem 'rspec'
-  gem 'webmock', require: false
-  gem 'vcr', require: false
+  gem "rspec"
+  gem "rubocop", require: false
+  gem "rubocop-rspec", require: false
   gem "standard", ">= 1.35.1", require: false
-  gem 'rubocop', require: false
-  gem 'rubocop-rspec', require: false
+  gem "vcr", require: false
+  gem "webmock", require: false
 end
--- lib/cafeznik/sources/local.rb
+++ lib/cafeznik/sources/local.rb
@@ -1,14 +1,23 @@
 require_relative "base"
+require "fast_ignore"
 
 module Cafeznik
   module Source
     class Local < Base
-      def tree = @_tree ||= ["./"] + Dir.glob("**/*").sort
+      def tree
+        @_tree ||= begin
+          paths = Dir.glob("**/*", File::FNM_DOTMATCH)
+          ignore = FastIgnore.new
+          paths.select { |path| ignore.allowed?(path, include_directories: true) }.sort
+        end
+      end
 
       def all_files = tree.reject { |path| File.directory?(path) }
 
       # TODO: check if this doubles the slashes (//)
-      def expand_dir(path) = Dir.glob("#{path.chomp('/')}/**/*").reject { |p| File.directory?(p) }
+      def expand_dir(path)
+        Dir.glob("#{path.chomp('/')}/**/*", File::FNM_DOTMATCH).reject { |p| File.directory?(p) }
+      end
 
       def dir?(path) = File.directory?(path)
 
