57fde73d0e70ea0fe9f71a21bb6fce436e0ede9a
Gill Abarbanel <SeventhHorror@gmail.com>
2024-10-25 10:04:57 +0200

    Polish local gitignore respect and make sure expand_dir respect it too!

--- lib/cafeznik/sources/local.rb
+++ lib/cafeznik/sources/local.rb
@@ -5,28 +5,29 @@ module Cafeznik
   module Source
     class Local < Base
       def tree
-        @_tree ||= begin
-          paths = Dir.glob("**/*", File::FNM_DOTMATCH)
-          ignore = FastIgnore.new
-          paths.select { |path| ignore.allowed?(path, include_directories: true) }.sort
-        end
+        @_tree ||= Dir.glob("**/*", File::FNM_DOTMATCH)
+                      .select { |path| gitignore.allowed?(path, include_directories: true) }
+                      .sort
       end
 
       def all_files = tree.reject { |path| File.directory?(path) }
 
-      # TODO: check if this doubles the slashes (//)
-      def expand_dir(path)
-        Dir.glob("#{path.chomp('/')}/**/*", File::FNM_DOTMATCH).reject { |p| File.directory?(p) }
-      end
+      def expand_dir(path) = Dir.glob("#{path.chomp('/')}/**/*", File::FNM_DOTMATCH)
+                                .reject { |p| File.directory?(p) || !gitignore.allowed?(p) }
 
       def dir?(path) = File.directory?(path)
 
+      # TODO: rename to file_contents
       def content(path)
         File.read(path)
       rescue Errno::ENOENT
         Log.error "File not found: #{path}"
         nil
       end
+
+      private
+
+      def gitignore = FastIgnore.new
     end
   end
 end
